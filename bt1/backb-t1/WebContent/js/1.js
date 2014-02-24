4d5fab4ef8af18206c4c4276ffa972f42794655f
T <T@T-comp1234.pl>
2014-02-24 11:22:40 +0100

    revert

---
+++ bt1/backb-t1/WebContent/js/1.js
@@ -0,0 +1,26 @@
+(function($){
+
+
+
+  var ListView = Backbone.View.extend({
+    el: $('body'), // attaches `this.el` to an existing element.
+
+    // initialize
+
+    initialize: function(){
+      _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+
+       this.render(); // not all views are self-rendering. This one is.
+    },
+
+    // render 3
+    
+    render: function(){
+      $(this.el).append("<ul> <li>hello world</li> </ul>");
+    }
+  });
+
+
+  var listView = new ListView();
+
+})(jQuery);
